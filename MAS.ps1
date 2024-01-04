41af383c0c4a0293af09a6c930660ede8f294470
Nilay Debnath <77850976+nilay-code@users.noreply.github.com>
2024-01-04 22:04:37 +0600

    Update MAS.ps1

--- MAS.ps1
+++ MAS.ps1
@@ -1,9 +1,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS_0.8.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/nilay-code/Windows-10-11-and-MS-office-Activation-Script/main/MAS_AIO.cmd'
 
-$FilePath = "$env:TEMP\IAS.cmd"
+$FilePath = "$env:TEMP\MAS.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
